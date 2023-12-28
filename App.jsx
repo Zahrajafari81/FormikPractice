96a2fb6423c359cd2c93092e7e1674be11a59146
Zahrajafari81 <139481132+Zahrajafari81@users.noreply.github.com>
2023-12-28 15:02:28 +0330

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import InputPage from "./components/InputPage";
+
+
+
+function App() {
+  return(
+
+ <InputPage />
+)
+  
+}
+
+export default App;
